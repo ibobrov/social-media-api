688c32304f5704d8b10ebc9e2ccc8f57d16f4921
ibobrov <coderilya@gmail.com>
2023-12-18 23:55:26 +1000

    Added SpringJdbcConfig

---
+++ src/main/kotlin/i/bobrov/social/config/SpringJdbcConfig.kt
@@ -0,0 +1,41 @@
+package i.bobrov.social.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+import javax.sql.DataSource
+
+@Configuration
+class SpringJdbcConfig {
+    @Value("\${spring.datasource.url}")
+    private lateinit var url: String
+
+    @Value("\${spring.datasource.username}")
+    private lateinit var username: String
+
+    @Value("\${spring.datasource.password}")
+    private lateinit var password: String
+
+    @Value("\${spring.datasource.driver-class-name}")
+    private lateinit var driver: String
+
+    @Bean
+    fun dataSource() =
+        DriverManagerDataSource().also {
+            it.setDriverClassName(driver)
+            it.url = url
+            it.username = username
+            it.password = password
+        }
+
+    @Bean
+    fun namedParameterJdbcOperations(dataSource: DataSource) =
+        NamedParameterJdbcTemplate(dataSource)
+
+    @Bean
+    fun transactionManager(dataSource: DataSource) =
+        DataSourceTransactionManager(dataSource)
+}
