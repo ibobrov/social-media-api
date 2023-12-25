54cacc522abbc518304d6e5ff0ffa5761a310d16
ibobrov <coderilya@gmail.com>
2023-12-25 18:47:08 +1000

    Drop lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.liquibase:liquibase-core")
-    compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
